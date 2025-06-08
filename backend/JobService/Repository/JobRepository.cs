42c6f4611f2d4fadf230cfb0bd41fc398f603d2d
Xin Qiao <qiaoxin628@gmail.com>
2025-06-07 20:30:48 -0400

    Add SQL pagination for backend

--- backend/JobService/Repository/JobRepository.cs
+++ backend/JobService/Repository/JobRepository.cs
@@ -13,14 +13,17 @@ namespace JobService.Repository;
 [Export(typeof(JobRepository))]
 public class JobRepository(DatabaseContext dbContext)
 {
+    private const int recentMonthThreshold = 2;
+
     /// <summary>
     /// Retrieves a job by ID
     /// </summary>
     /// <param name="jobId">The id of the job</param>
+    /// <param name="token">A token to monitor for cancellation requests</param>
     /// <returns>
     /// A <see cref="Job?"/> if found; otherwise, <c>null</c>
     /// </returns>
-    public async ValueTask<Job?> GetByIdAsync(Guid jobId)
+    public async ValueTask<Job?> GetByIdAsync(Guid jobId, CancellationToken token)
     {
         byte[] rawId = MySqlGuidConverter.GuidToMySqlBinary(jobId);
 
@@ -28,7 +31,7 @@ public class JobRepository(DatabaseContext dbContext)
         (
             $"SELECT * FROM JOB WHERE ID = {rawId}"
         )
-        .ToListAsync();
+        .ToListAsync(token);
 
         if (jobs.IsNullOrEmpty() || jobs.Count > 1)
         {
@@ -42,8 +45,11 @@ public class JobRepository(DatabaseContext dbContext)
     /// Retrieves a list of jbos that the user has interest
     /// </summary>
     /// <param name="userId">The user who has interet</param>
+    /// <param name="skip">the number of jobs to skip from the result</param>
+    /// <param name="take">the number of jobs to return from the result</param>
+    /// <param name="token">A token to monitor for cancellation requests</param>
     /// <returns>A list of jobs that the user has interest</returns>
-    public async ValueTask<IReadOnlyList<Job>> GetUserInteredJobsAsync(Guid userId)
+    public async ValueTask<IReadOnlyList<Job>> GetUserInteredJobsAsync(Guid userId, int skip, int take, CancellationToken token)
     {
         byte[] rawId = MySqlGuidConverter.GuidToMySqlBinary(userId);
 
@@ -53,9 +59,11 @@ public class JobRepository(DatabaseContext dbContext)
                 SELECT j.* FROM INTERESTEDJOB i
                   LEFT JOIN JOB j
                     ON i.JOB_ID = j.ID
-                 WHERE USER_ID = {rawId}"
+                 WHERE USER_ID = {rawId}
+                 ORDER BY DATE_POSTED DESC
+                 LIMIT {take} OFFSET {skip}"
         )
-        .ToListAsync();
+        .ToListAsync(token);
 
         if (jobs.IsNullOrEmpty())
         {
@@ -69,8 +77,11 @@ public class JobRepository(DatabaseContext dbContext)
     /// Retrieves a list of jbos that posted by the user
     /// </summary>
     /// <param name="userId">The user who posted the jobs</param>
+    /// <param name="skip">the number of jobs to skip from the result</param>
+    /// <param name="take">the number of jobs to return from the result</param>
+    /// <param name="token">A token to monitor for cancellation requests</param>
     /// <returns>A list of jobs that has been posted by the user</returns>
-    public async ValueTask<IReadOnlyList<Job>> GetUserPostedJobsAsync(Guid userId)
+    public async ValueTask<IReadOnlyList<Job>> GetUserPostedJobsAsync(Guid userId, int skip, int take, CancellationToken token)
     {
         byte[] rawId = MySqlGuidConverter.GuidToMySqlBinary(userId);
 
@@ -78,9 +89,41 @@ public class JobRepository(DatabaseContext dbContext)
         (
             $@"
                 SELECT * FROM JOB
-                 WHERE POSTED_BY_USER = {rawId}"
+                 WHERE POSTED_BY_USER = {rawId}
+                 ORDER BY DATE_POSTED DESC
+                 LIMIT {take} OFFSET {skip}"
         )
-        .ToListAsync();
+        .ToListAsync(token);
+
+        if (jobs.IsNullOrEmpty())
+        {
+            return [];
+        }
+
+        return jobs;
+    }
+
+    /// <summary>
+    /// Get a paged result of all jobs
+    /// </summary>
+    /// <param name="skip">the number of jobs to skip from the result</param>
+    /// <param name="take">the number of jobs to return from the result</param>
+    /// <param name="token">A token to monitor for cancellation requests</param>
+    /// <returns>A list of jobs</returns>
+    public async ValueTask<IReadOnlyList<Job>> GetPaged(int skip, int take, CancellationToken token)
+    {
+        var cutoffDate = DateTime.UtcNow.AddMonths(recentMonthThreshold);
+
+        var jobs = await dbContext.Jobs.FromSqlInterpolated
+        (
+            $@"
+            SELECT * 
+              FROM JOB
+             WHERE DATE_POSTED >= {cutoffDate}
+             ORDER BY DATE_POSTED DESC
+             LIMIT {take} OFFSET {skip}"
+        )
+        .ToListAsync(token);
 
         if (jobs.IsNullOrEmpty())
         {
--- backend/UserService/Controllers/UserController.cs
+++ backend/UserService/Controllers/UserController.cs
@@ -16,18 +16,18 @@ public class UserController(UserRepository userRepository) : ControllerBase
     /// <summary>
     /// Find a <see cref="User"/> by the username
     /// </summary>
-    /// <param name="username">The username to find the <see cref="User"/></param>
-    /// <param name="password">The password to find the <see cref="User"/></param>
+    /// <param name="authenticationRequest">The username and password to find the <see cref="User"/></param>
+    /// <param name="token">A token to monitor for cancellation requests</param>
     /// <returns>A <see cref="User"/> if found; otherwise <c>null</c></returns>
     [HttpPost("GetUser")]
-    public async ValueTask<IActionResult> GetAsync([FromBody] AuthenticationRequest authenticationRequest)
+    public async ValueTask<IActionResult> GetAsync([FromBody] AuthenticationRequest authenticationRequest, [FromRoute] CancellationToken token)
     {
         if (authenticationRequest.Username.IsNullOrEmpty() || authenticationRequest.Password.IsNullOrEmpty())
         {
             return BadRequest("No username or password specified!");
         }
 
-        var user = await userRepository.GetAsync(authenticationRequest.Username, authenticationRequest.Password);
+        var user = await userRepository.GetAsync(authenticationRequest.Username, authenticationRequest.Password, token);
 
         if (user is null)
         {
